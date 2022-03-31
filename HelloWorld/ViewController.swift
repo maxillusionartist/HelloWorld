4c5b148730562bc2b7cb288c874a594a4ea36893
MAX ILLUSION <maxillusionmusic@mail.ru>
2022-03-31 12:58:25 -0400

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Maksim Iliukhin on 3/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
